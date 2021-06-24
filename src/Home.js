4aa935a8ec8427045e2b9e7e8ade04d21dde73c2
mohamed abdelwahab <moh.elwah@gmail.com>
2021-06-25 01:07:41 +0200

    add props to blogs layout

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,17 @@
 import { useState } from 'react';
+import BlogList from './Bloglist';
 
 const Home = () => {
  const [blogs, setBlogs] = useState([
     {title: 'My new web site', body: 'Lorem ', author: 'mohamed', id: 1},
-    {title: 'First list in react', body: 'Lorem ', author: 'mohamed', id: 2},
+    {title: 'First list in react', body: 'Lorem ', author: 'Ahmed', id: 2},
     {title: 'make adynamic list', body: 'Lorem ipsum ', author: 'mohamed', id: 3}
  ]);
 
     return ( 
         <div className="Home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>written by { blog.author }</p>
-                </div>
-            ))}
-            
+            <BlogList blogs = { blogs } title = "All Blogs List !"/>
+            <BlogList blogs = { blogs.filter( (blog) => blog.author === 'mohamed' ) } title = "Mohamed Blogs List !"/>
         </div>
      );
 }
