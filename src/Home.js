0b2047fbc02efc4cb5a08d119ff4985a11471358
mohamed abdelwahab <moh.elwah@gmail.com>
2021-06-25 12:22:15 +0200

    fetch data from local json server

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,23 @@ import { useState, useEffect } from 'react';
 import BlogList from './Bloglist';
 
 const Home = () => {
- const [blogs, setBlogs] = useState([
-    {title: 'My new web site', body: 'Lorem ', author: 'mohamed', id: 1},
-    {title: 'First list in react', body: 'Lorem ', author: 'Ahmed', id: 2},
-    {title: 'make adynamic list', body: 'Lorem ipsum ', author: 'mohamed', id: 3}
- ]);
+ const [blogs, setBlogs] = useState(null);
 
- const handleDelete = (id) => {
-    const newBlogs = blogs.filter (blog => blog.id !== id );
-    setBlogs(newBlogs);
- }
+
 
 useEffect (() => {
-     console.log(blogs);
- });
+    fetch('http://localhost:8000/blogs')
+      .then (res => {
+         return res.json();
+      })
+      .then (data => {
+         setBlogs(data);
+      })
+ }, []);
     return ( 
         <div className="Home">
-            <BlogList blogs = { blogs } title = "All Blogs List !" handleDelete = {handleDelete}/>
-            <BlogList blogs = { blogs.filter( (blog) => blog.author === 'mohamed' ) }   handleDelete = {handleDelete} title = "Mohamed Blogs List !" />
+            {blogs && <BlogList blogs = { blogs } title = "All Blogs List !" />
+         }
         </div>
      );
 }
