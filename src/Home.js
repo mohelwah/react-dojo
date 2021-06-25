2dbf8d1b2ff758586dfab9bd8058df1bd42c27c2
mohamed abdelwahab <moh.elwah@gmail.com>
2021-06-25 12:29:14 +0200

    add Loading option to page by using isPending

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,26 @@ import BlogList from './Bloglist';
 
 const Home = () => {
  const [blogs, setBlogs] = useState(null);
+ const [isPending, setIsPending] = useState(true);
 
 
 
 useEffect (() => {
-    fetch('http://localhost:8000/blogs')
+   setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
       .then (res => {
          return res.json();
       })
       .then (data => {
          setBlogs(data);
+         setIsPending(false);
       })
- }, []);
+
+   }, 1000);
+}, []);
     return ( 
         <div className="Home">
+           {isPending && <div>Loading .....</div>}
             {blogs && <BlogList blogs = { blogs } title = "All Blogs List !" />
          }
         </div>
